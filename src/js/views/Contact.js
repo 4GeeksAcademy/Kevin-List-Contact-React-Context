bb670cd2dc245bec1836abc9d49b25dac8376b61
Kevin Villafuerte Montero <116960904+Kevin962022@users.noreply.github.com>
2024-08-20 21:54:47 +0000

    acabado

---
+++ src/js/views/Contact.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useContext } from "react";
+import { Context } from "../store/appContext.js";
+import { ContactForm } from "../component/ContactForm.js";
+import { Navbar } from "../component/navbar.js";
+
+export const Contact = () => {
+    const { store, actions } = useContext(Context);
+
+    useEffect(() => {
+        actions.fetchContactos();
+    }, [actions]);
+
+    const handleDelete = (id) => {
+        const confirmDelete = window.confirm("¿Estás seguro de que deseas eliminar el contacto?");
+        if (confirmDelete) {
+            actions.deleteContact(id);
+        }
+    };
+
+    return (
+        <>
+            <Navbar />
+            <div>
+                {store.contactos.map((contact, index) => (
+                    <ContactForm key={index} contact={contact} contactDelete={handleDelete} />
+                ))}
+            </div>
+        </>
+    );
+};
